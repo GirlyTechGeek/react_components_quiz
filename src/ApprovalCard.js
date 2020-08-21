1e9dcd63462d73aec0d2a6f1cf2ff94b5c539544
GirlyTechGeek <53258022+GirlyTechGeek@users.noreply.github.com>
2020-08-21 22:55:48 +0000

    Update ApprovalCard.js

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -24,13 +24,14 @@ class ApprovalCard extends React.Component{
   render(){
     const {props} = this.props;
     return (
-       <div>
-         <p className="ui card" ></p>
-         <p className="content">{this.children}</p>
-         <p className="extra content"></p>
-         <p className="ui two buttons"></p>
-         <p className="ui basic green button">Approve</p>
-         <p className="ui basic red button">Reject</p>
+      <div className="ui card">
+        <div className="content">{props.children}</div>
+        <div className="extra content">
+          <div className="ui two buttons">
+            <div className="ui basic green button">Approve</div>
+            <div className="ui basic red button">Reject</div>
+          </div>
+        </div>
       </div>
     );
   }
